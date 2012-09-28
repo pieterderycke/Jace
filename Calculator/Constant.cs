d2900f4f5dba1dbbc9e173430d0bb3bf9bd2ef07
Pieter De Rycke <pieter_de_rycke@hotmail.com>
2012-09-28 08:24:29 +0200

    Initial commit of Calculator

---
+++ Calculator/Constant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    public class Constant<T>
+    {
+        private T value;
+
+        public Constant(T value)
+        {
+            this.value = value;
+        }
+    }
+}
