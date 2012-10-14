ffa3936dfc48305a4a7a4674f5991096f6fe4f04
Pieter De Rycke <pieter_de_rycke@hotmail.com>
2012-10-14 12:11:57 +0200

    moved variable not defined exception to root namespace

--- Calculator/VariableNotDefinedException.cs
+++ Calculator/VariableNotDefinedException.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Calculator.Operations
+namespace Calculator
 {
     /// <summary>
     /// An exception thrown when a formula must be executed with a variable that is not defined.
