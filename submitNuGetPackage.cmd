a228f5c0152fcd433da089cb00a5bb3983c79507
Pieter De Rycke <pieter_de_rycke@hotmail.com>
2012-11-22 07:12:47 +0100

    improved nuget submit script

--- submitNuGetPackage.cmd
+++ submitNuGetPackage.cmd
@@ -1,9 +1,7 @@
-msbuild /p:Configuration=Release Jace\Jace.csproj
-Tools\NuGet\nuget.exe pack Jace\Jace.csproj -Prop Configuration=Release
+buildNuGetPackage.cmd
 
 @ECHO OFF
 FOR /F "delims=|" %%I IN ('DIR "Jace.*.nupkg" /B /O:D') DO SET NuGetPackage=%%I
 @ECHO ON
 
-REM ECHO %NuGetPackage%
 Tools\NuGet\nuget.exe push %NuGetPackage%
