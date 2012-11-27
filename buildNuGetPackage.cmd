7c4f4a204cbb7dcc551c20bdd71845b1b2ecd965
Pieter De Rycke <pieter_de_rycke@hotmail.com>
2012-11-27 07:14:07 +0100

    added support for WP8 in the nuget build script

--- buildNuGetPackage.cmd
+++ buildNuGetPackage.cmd
@@ -1,6 +1,5 @@
-SET version="0.6"
+SET version="0.7"
 
-Tools\patch.exe Jace\Properties\AssemblyInfo.cs nuget\AssemblyInfo.Patch
 msbuild /p:Configuration=Release Jace\Jace.csproj
 MKDIR nuget\lib\net40
 COPY Jace\bin\Release\*.dll nuget\lib\net40\
@@ -8,6 +7,10 @@ COPY Jace\bin\Release\*.dll nuget\lib\net40\
 msbuild /p:Configuration=Release Jace.WinRT\Jace.WinRT.csproj
 MKDIR nuget\lib\windows8
 COPY Jace.WinRT\bin\Release\*.dll nuget\lib\windows8\
+
+msbuild /p:Configuration=Release Jace.WP8\Jace.WP8.csproj
+MKDIR nuget\lib\wp8
+COPY Jace.WP8\bin\Release\*.dll nuget\lib\wp8\
 
 COPY Jace.nuspec nuget\
 
