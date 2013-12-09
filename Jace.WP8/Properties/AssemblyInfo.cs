61a52fdd51bbec35fde0312cb6ffe7413279249d
Pieter De Rycke <pieter_de_rycke@hotmail.com>
2013-12-09 18:25:32 +0100

    Upgraded the version number to 0.8.5

--- Jace.WP8/Properties/AssemblyInfo.cs
+++ Jace.WP8/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.8.4")]
-[assembly: AssemblyFileVersion("0.8.4")]
+[assembly: AssemblyVersion("0.8.5")]
+[assembly: AssemblyFileVersion("0.8.5")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
